52af22260c80934c63e229e851bd48b45b4701f9
alamhanz <alamhanz@gmail.com>
2017-02-17 00:15:58 +0700

    debug

--- strike_ball.py
+++ strike_ball.py
@@ -7,26 +7,27 @@ print "save 4 different number between 0-9"
 #All_Possible_Answer
 A=list(permut([i+1 for i in range(9)],4))
 gn=1
-choose_a=np.random.randint(len(A))
-guess=A[choose_a]
 score_board=pd.Series([-1]*len(A),index=A)
+ans=[0,-1]
 
 while ans[0]!=4:
 	print "---------------------"
 	print "guess number ",gn
 	print "---------------------"
 
-	print guess
+	if sum(ans)==-1:
+		choose_a=np.random.randint(len(A))
+		guess=A[choose_a]
 
-	ans=input("is it true? [strike,ball] :")
-
-	if sum(ans)==0:
+	elif sum(ans)==0:
 		#MassiveElimination
 		g=set(guess)
 		A=list(permut(list(set([i+1 for i in range(9)])-g),4))
 		#another_guess
+		choose_a=np.random.randint(len(A))
+		guess=A[choose_a]
+		#another_guess
 		
-
 	elif sum(ans)==4:
 		if len(A)<=24:
 			#Elimination
@@ -42,3 +43,8 @@ while ans[0]!=4:
 		A.remove(guess)
 		#new_scoring
 		#another_guess
+
+	print guess
+
+	ans=input("is it true? [strike,ball] :")
+	gn+=1
